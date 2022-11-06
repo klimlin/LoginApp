1909b37c26ff8fa6f400b6e348566d19e339b2c2
Alina <linalevel5@gmail.com>
2022-11-06 17:47:03 +0300

    Interface setup

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by MAcbook on 06.11.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
